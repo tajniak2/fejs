11c21a5e8024434f2a88646d07359b6c0b484e8c
tajniak <mdz2@o2.pl>
2013-07-09 12:37:45 +0200

    Fixed adding friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,14 @@
 ﻿class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @friendship = current_user.friendships.build(userB_id: params[:friend_id])
+    @firendship_rev = Friendship.find_by_userA_id_and_userB_id(params[:friend_id], current_user)
+    if @firendship_rev
+      @firendship_rev.accepted = true
+      @firendship_rev.save
+      @friendship.accepted = true
+    end
     if @friendship.save
-      flash[:notice] = "Wysłano zaproszenie"
+      flash[:notice] = "Wysłano zaproszenie lub zaakceptowano znajomość"
       redirect_to root_url
     else
       flash[:error] = "Coś nie tak..."
@@ -21,10 +27,10 @@
   #  @tweets = Tweets.from_friens(current_user)
   #end
   
-  #def index
-  #  @user = current_user
-  #  @users = current_user.requests
-  #end
+  def index
+    @user = current_user
+    @users = current_user.requests
+  end
   
   #def accept
   #  ship = Friendship.find_all_by_userA_id(params[:userB_id]).find_by_userB_id(current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,12 @@ Facebook::Application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
-  match 'feed', to: 'firendship#feed', as: 'feed'
-  match 'requests', to: 'firendship#index', as: 'requests'
-  match 'accept/:userB_id', to: 'firendship#accept', as: 'accept'
+  match 'feed', to: 'friendships#feed', as: 'feed'
+  match 'requests', to: 'friendships#index', as: 'requests'
+  #match 'accept/:userB_id', to: 'friendships#accept', as: 'accept'
   
-  match 'users/:id/add', to: 'firendship#add', as: 'add_user_to_friends'
-  match 'users/:id/del', to: 'firendship#del', as: 'delete_user_from_friends'
+  #match 'users/:id/add', to: 'friendship#add', as: 'add_user_to_friends'
+  #match 'users/:id/del', to: 'friendship#del', as: 'delete_user_from_friends'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
