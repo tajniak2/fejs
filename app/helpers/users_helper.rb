b8e85e63495bd566509f5d685fad8ab696ac7f21
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-10 11:55:40 +0200

    Fixed addding friends link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-    def can_be_added?(added)
-        current_user and (added != current_user) and !Friendship.where(userA_id: current_user.id, userB_id: added.id)
-    end
+  def can_be_added?(added)
+    current_user and (added != current_user) and !Friendship.where(userA_id: current_user.id, userB_id: added.id).exists?
+  end
 end
