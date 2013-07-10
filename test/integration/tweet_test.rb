9e2346dc7f7ad3666cd76bea785e4b02448b32e1
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-10 12:04:58 +0200

    Added tweet integeration tests

---
+++ test/integration/tweet_test.rb
@@ -0,0 +1,18 @@
+﻿require 'test_helper'
+
+class TweetTest < ActionDispatch::IntegrationTest
+  test "save & show tweet" do
+    user = FactoryGirl.create(:user1)
+    visit root_path
+    click_on 'Zaloguj'
+    fill_in 'Adres e-mail', with: 'ktos@cos.pl' 
+    fill_in 'Hasło', with: '1'
+    click_button 'Zaloguj'
+    
+    visit root_path
+    click_on 'Dodaj wpis'
+    fill_in 'Status', with: 'Taki sobie tweet' 
+    click_on 'Dodaj'
+    assert has_content?('ktos@cos.pl: Taki sobie tweet')
+  end  
+end
