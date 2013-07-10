40cef2103ffd106a7e2fe74b0c54953fa585ab50
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-10 11:39:46 +0200

    Changed way of showing adding friend link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-    def can_be_added?(added, current)
-        current and (added != current) and !Friendship.where(userA_id: current.id, userB_id: added.id)
+    def can_be_added?(added)
+        current_user and (added != current_user) and !Friendship.where(userA_id: current_user.id, userB_id: added.id)
     end
 end
