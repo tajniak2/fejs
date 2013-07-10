ac050192c21cb9441711af6fcc570ce977f624ad
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-10 12:22:49 +0200

    Added friendship integeration test

---
+++ test/integration/friendship_test.rb
@@ -0,0 +1,20 @@
+﻿require 'test_helper'
+
+class FriendshipTest < ActionDispatch::IntegrationTest
+  def log_in(email)
+    visit root_path
+    click_on 'Zaloguj'
+    fill_in 'Adres e-mail', with: email 
+    fill_in 'Hasło', with: '1'
+    click_button 'Zaloguj'
+  end
+
+  test "add friend" do
+    user_1 = FactoryGirl.create(:user_0)
+    user_2 = FactoryGirl.create(:user_0)
+    log_in user_1.email
+    visit root_path
+    click_link 'Dodaj znajomego'
+    assert has_content?('Wysłano zaproszenie'), "invitation hasn't been sent"
+  end
+end
