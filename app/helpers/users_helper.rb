b6ad3649437d1c190a245a1594e5ff0325d8347f
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-10 10:47:59 +0200

    Fixed user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
     def can_be_added?(added, current)
-        current.nil? and (added != current) and !Friendship.where(userA_id: current.id, userB_id: added.id)
+        current and (added != current) and !Friendship.where(userA_id: current.id, userB_id: added.id)
     end
 end
