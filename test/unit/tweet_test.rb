0f0f3db4cd914346525a0acead9c35eeba154a64
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-11 12:24:30 +0200

    Fixed updating test

--- test/unit/tweet_test.rb
+++ test/unit/tweet_test.rb
@@ -30,10 +30,11 @@ class TweetTest < ActiveSupport::TestCase
   
   test "if save_update method updates tweet properly" do
     @tweet.save_new
-    tweet_db = Tweet.find(@tweet.id)
-    assert_equal "Fajny status", tweet_db.status, "tweet's status set incorrectly"
-    assert_equal 1, tweet_db.version, "tweet's version set incorrectly"
-    assert_equal @tweet.id, tweet_db.tweet_id, "tweet's id set incorrectly"
-    assert tweet_db.current, "tweet's current set incorrectly"
+    tweet_new = @tweet.save_update(@user, {status: "Fajny"} )
+    assert_equal "Fajny", tweet_new.status, "tweet_new's status set incorrectly"
+    assert_equal @tweet.version + 1, tweet_new.version, "tweet_ne's version set incorrectly"
+    assert_equal @tweet.tweet_id, tweet_new.tweet_id, "tweet_new's id set incorrectly"
+    assert tweet_new.current, "tweet_new's current set incorrectly"
+    assert !@tweet.current, "old tweet's current set incorrectly"
   end
 end
