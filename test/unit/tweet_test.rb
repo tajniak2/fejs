3413303b19a0ac830b38c734d57abe6b77c4d151
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-11 11:30:38 +0200

    Added tweet's unit tests

--- test/unit/tweet_test.rb
+++ test/unit/tweet_test.rb
@@ -15,7 +15,25 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = FactoryGirl.create(:user_1)
+    @tweet = @user.tweets.build(status: "Fajny status")
+  end
+
+  test "if save_new method saves tweet properly" do
+    @tweet.save_new
+    tweet_db = Tweet.find(tweet.id)
+    assert_equal 1, tweet_db.version, "tweet's version set incorrectly"
+    assert_equal @tweet.id, tweet_db.tweet_id, "tweet's id set incorrectly"
+    assert tweet_db.current, "tweet's current set incorrectly"
+  end
+  
+  test "if save_update method updates tweet properly" do
+    @tweet.save_new
+    tweet_db = Tweet.find(tweet.id)
+    assert_equal "Fajny status", tweet_db.status, "tweet's status set incorrectly"
+    assert_equal 1, tweet_db.version, "tweet's version set incorrectly"
+    assert_equal @tweet.id, tweet_db.tweet_id, "tweet's id set incorrectly"
+    assert tweet_db.current, "tweet's current set incorrectly"
+  end
 end
