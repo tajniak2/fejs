1dbcd16a91bf645f5abdbf55cdf175227cce9ff3
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-12 13:51:41 +0200

    added accept invitation link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,8 @@
 module UsersHelper
+  def has_invitation?(user)
+    current_user and current_user.requests.where(id: user.id).exists?
+  end
+
   def can_be_added?(added)
     current_user and (added != current_user) and !Friendship.where(userA_id: current_user.id, userB_id: added.id).exists?
   end
