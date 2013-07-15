a6e00e8411f73d5c6009a5716fcb150b092739fc
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-15 09:15:43 +0200

    Added belogs_to :userB

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -14,7 +14,7 @@ class Friendship < ActiveRecord::Base
   attr_accessible :userA_id, :userB_id, :accepted
   
   belongs_to :userA, class_name: "User"
-  # belongs_to :userB_id, class_name: "User"
+  belongs_to :userB, class_name: "User" 
   
   validate :friendship_validator
  
