a451633cc766c0f5b66a36fd3073643d8e86259b
Maciej Dzieżyc <maciej@maciej-VirtualBox.(none)>
2013-07-16 14:14:57 +0200

    Added showing number of unread activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -20,4 +20,9 @@ class Activity < ActiveRecord::Base
     friends_ids = Friendship.where(userA_id: user.id, accepted: true).map(&:userB_id)
     where("user_id IN (?) OR user_id = ?", friends_ids, user.id).order("created_at desc")
   end
+  
+  def self.how_many?(user)
+    friends_ids = Friendship.where(userA_id: user.id, accepted: true).map(&:userB_id)
+    where("(user_id IN (?) OR user_id = ?) AND created_at > ?", friends_ids, user.id, user.seen_feed).count
+  end
 end
